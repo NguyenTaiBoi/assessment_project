633ae9202f8d4903f81f6218691433e08418f2d6
BoiNT <BoiNT@splus-software.com.vn>
2021-11-04 00:02:42 +0700

    [Update]Update assigned card

--- accessment_mobile_project/lib/screen/dash_board/dash_board_screen.dart
+++ accessment_mobile_project/lib/screen/dash_board/dash_board_screen.dart
@@ -38,56 +38,171 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
                                   color: Colors.grey,
                                   blurRadius: 5)
                             ]),
-                        child: Column(
-                          children: [
-                            Text("Title"),
-                            SingleChildScrollView(
-                              scrollDirection: Axis.horizontal,
-                              child: Row(
-                                children: [
-                                  Container(
-                                    height: 50,
-                                    width: 50,
-                                    child: TimelineTile(
-                                      axis: TimelineAxis.horizontal,
-                                      alignment: TimelineAlign.start,
-                                      endChild: Container(
-                                        constraints: const BoxConstraints(
-                                          minWidth: 50,
-                                        ),
-                                        color: Colors.lightGreenAccent,
-                                      ),
-                                    ),
-                                  ),
-                                  Container(
-                                    height: 50,
-                                    width: 50,
-                                    child: TimelineTile(
-                                      indicatorStyle:
-                                          IndicatorStyle(color: Colors.green),
-                                      axis: TimelineAxis.horizontal,
-                                      alignment: TimelineAlign.start,
-                                      endChild: Container(
-                                        constraints: const BoxConstraints(
-                                          minWidth: 50,
-                                        ),
-                                        color: Colors.lightGreenAccent,
-                                      ),
-                                    ),
-                                  ),
-                                  Container(
-                                    height: 50,
-                                    width: 50,
-                                    child: TimelineTile(
-                                      axis: TimelineAxis.horizontal,
-                                      alignment: TimelineAlign.start,
-                                      endChild: Icon(Icons.access_alarm),
-                                    ),
-                                  )
-                                ],
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: Colors.blue, width: 2),
+                                    borderRadius: BorderRadius.circular(15)),
+                                child: Text(
+                                  "Manger confirmed",
+                                  style: TextStyle(
+                                      color: Colors.blue, fontSize: 18),
+                                ),
                               ),
-                            ),
-                          ],
+                              Center(
+                                child: Container(
+                                  height: 100,
+                                  child: SingleChildScrollView(
+                                    scrollDirection: Axis.horizontal,
+                                    child: Row(
+                                      children: [
+                                        Container(
+                                          height: 50,
+                                          width: 80,
+                                          child: TimelineTile(
+                                            beforeLineStyle:
+                                                LineStyle(color: Colors.red),
+                                            indicatorStyle: IndicatorStyle(
+                                                width: 30,
+                                                height: 30,
+                                                color: Colors.red),
+                                            axis: TimelineAxis.horizontal,
+                                            alignment: TimelineAlign.start,
+                                            endChild: Container(),
+                                          ),
+                                        ),
+                                        Container(
+                                          height: 50,
+                                          width: 80,
+                                          child: TimelineTile(
+                                            beforeLineStyle:
+                                                LineStyle(color: Colors.red),
+                                            indicatorStyle: IndicatorStyle(
+                                                width: 30,
+                                                height: 30,
+                                                color: Colors.red),
+                                            axis: TimelineAxis.horizontal,
+                                            alignment: TimelineAlign.start,
+                                            endChild: Container(),
+                                          ),
+                                        ),
+                                        Container(
+                                          height: 50,
+                                          width: 80,
+                                          child: TimelineTile(
+                                            indicatorStyle: IndicatorStyle(
+                                                width: 30, height: 30),
+                                            axis: TimelineAxis.horizontal,
+                                            alignment: TimelineAlign.start,
+                                            endChild: Container(),
+                                          ),
+                                        ),
+                                        Container(
+                                          height: 50,
+                                          width: 80,
+                                          child: TimelineTile(
+                                            indicatorStyle: IndicatorStyle(
+                                                color: Colors.green,
+                                                width: 30,
+                                                height: 30),
+                                            axis: TimelineAxis.horizontal,
+                                            alignment: TimelineAlign.start,
+                                            endChild: Icon(
+                                              Icons.person,
+                                              color: Colors.green,
+                                              size: 40,
+                                            ),
+                                          ),
+                                        ),
+                                        Container(
+                                          height: 50,
+                                          width: 80,
+                                          child: TimelineTile(
+                                            indicatorStyle: IndicatorStyle(
+                                                width: 30, height: 30),
+                                            axis: TimelineAxis.horizontal,
+                                            alignment: TimelineAlign.start,
+                                            endChild: Container(),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                  "[ID001][Frozen][Type01] Check the extent of damage of frozen seafood",
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 18)),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      RichText(
+                                          text: TextSpan(
+                                              style: TextStyle(
+                                                  color: Colors.black,
+                                                  fontSize: 18),
+                                              text: "Start date: ",
+                                              children: [
+                                            TextSpan(text: "01/10/2021")
+                                          ])),
+                                      SizedBox(
+                                        height: 5,
+                                      ),
+                                      RichText(
+                                          text: TextSpan(
+                                              style: TextStyle(
+                                                  color: Colors.black,
+                                                  fontSize: 18),
+                                              text: "Due date: ",
+                                              children: [
+                                            TextSpan(text: "01/12/2021")
+                                          ])),
+                                    ],
+                                  ),
+                                  Spacer(),
+                                  Column(
+                                    children: [
+                                      Text("3 days",
+                                          style: TextStyle(
+                                              color: Colors.black,
+                                              fontSize: 18)),
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      Container(
+                                        padding: EdgeInsets.symmetric(
+                                            vertical: 5, horizontal: 15),
+                                        decoration: BoxDecoration(
+                                            color: Colors.green[300],
+                                            borderRadius:
+                                                BorderRadius.circular(15)),
+                                        child: Text(
+                                          "Accessment",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 18),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     ),
